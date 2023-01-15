c874f1b3e8ddf39f8f68db8aa71c3de364e0c68a
Mirthe Valentijn <mirthe@aanzee.nl>
2023-01-15 17:01:38 +0100

    Error handling

--- index.php
+++ index.php
@@ -21,39 +21,45 @@
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                 $rawdata = curl_exec($ch);
                 curl_close($ch);
-                $completebookinfo = json_decode($rawdata,true);
+                $completebookinfo = json_decode($rawdata, true);
                 // print_r($completebookinfo); exit();
-                $bookinfo = $completebookinfo['items'][0]['volumeInfo'];
+                if (isset($completebookinfo['items'])) {
+                    $bookinfo = $completebookinfo['items'][0]['volumeInfo'];
 
-                // TODO offer both links?
-                // $booklink = $bookinfo['canonicalVolumeLink'];
-                $booklink = "https://www.goodreads.com/search?q=". $isbn;
+                    // TODO offer both links?
+                    // $booklink = $bookinfo['canonicalVolumeLink'];
+                    $booklink = "https://www.goodreads.com/search?q=". $isbn;
 
-                $mijnoutput = '<div class="well">';
-                if (array_key_exists('imageLinks',$bookinfo)){
-                    $mijnoutput .= '<div class="well-img"><img src="'.str_replace('http://','https://',$bookinfo['imageLinks']['thumbnail']).'" alt="" width="128"></div>';
-                }
-                $mijnoutput .= '<div class="well-body">';
-                $mijnoutput .= '<p><a href="'.$booklink.'">'.$bookinfo['title']."</a> - ".$bookinfo['authors'][0]."<br>";
-                $mijnoutput .= 'Gepubliceerd '. $bookinfo['publishedDate'] ." &bull; ";
-                $mijnoutput .= $bookinfo['pageCount']." pagina's</p>";
-
-                if (array_key_exists('description',$bookinfo)){
-                    $mijnoutput .= '<p>'.mb_strimwidth($bookinfo['description'],0,350, '&#8230;')."</p>";
-                    // TODO add collapse if text is longer
-                }
-
-                if (array_key_exists('categories',$bookinfo)){
-                    $i = 0;
-                    $mijnoutput .= "<ul class=\"genres\">";
-                    foreach ($bookinfo['categories'] as $genre) {
-                        $mijnoutput .= '<li>'. $genre . "</li>";
-                        if (++$i == 5) break;
+                    $mijnoutput = '<div class="well">';
+                    if (array_key_exists('imageLinks', $bookinfo)) {
+                        $mijnoutput .= '<div class="well-img"><img src="'.str_replace('http://', 'https://', $bookinfo['imageLinks']['thumbnail']).'" alt="" width="128"></div>';
                     }
-                    $mijnoutput .= "</ul>";
-                }
+                    $mijnoutput .= '<div class="well-body">';
+                    $mijnoutput .= '<p><a href="'.$booklink.'">'.$bookinfo['title']."</a> - ".$bookinfo['authors'][0]."<br>";
+                    $mijnoutput .= 'Gepubliceerd '. $bookinfo['publishedDate'] ." &bull; ";
+                    $mijnoutput .= $bookinfo['pageCount']." pagina's</p>";
 
-                $mijnoutput .= '</div></div>';
+                    if (array_key_exists('description', $bookinfo)) {
+                        $mijnoutput .= '<p>'.mb_strimwidth($bookinfo['description'], 0, 350, '&#8230;')."</p>";
+                        // TODO add collapse if text is longer
+                    }
+
+                    if (array_key_exists('categories', $bookinfo)) {
+                        $i = 0;
+                        $mijnoutput .= "<ul class=\"genres\">";
+                        foreach ($bookinfo['categories'] as $genre) {
+                            $mijnoutput .= '<li>'. $genre . "</li>";
+                            if (++$i == 5) {
+                                break;
+                            }
+                        }
+                        $mijnoutput .= "</ul>";
+                    }
+
+                    $mijnoutput .= '</div></div>';
+                } else {
+                    $mijnoutput = '<p>Error in Bookblock..</p>';
+                }
                
                 return $mijnoutput;
             }
